b74ed8c3e309fe6cdd599634001c90b4f12a5cff
alisherka2 <alisherdavletov06@icloud.com>
2026-04-24 18:13:29 +0500

    Add files via upload

---
+++ prac7/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "database": "phonebookdb",
+    "user": "postgres",
+    "password": "AliLoh123"  # Change this to your PostgreSQL password
+}
